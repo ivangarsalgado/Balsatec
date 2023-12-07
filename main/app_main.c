9e239e5895ccd5a460328225d281d9755a8dd0c5
ivangarsalgado <ivan.garcia.salgado@alumnos.upm.es>
2023-12-07 15:04:50 +0100

    Commit for OTA

---
+++ main/app_main.c
@@ -0,0 +1,393 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "driver/adc.h"
+#include "driver/gpio.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "cJSON.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "esp_timer.h"
+#include "driver/uart.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+#include "ds18b20.h"
+
+#include "driver/gpio.h"
+
+static const char *TAG = "MQTT_EXAMPLE";
+#define EXAMPLE_ESP_WIFI_SSID      "Sorivanyo"
+#define EXAMPLE_ESP_WIFI_PASS      "30160127"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+
+#define TEMP_BUS 17
+
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (GPIO_NUM_1)
+#define RXD_PIN (GPIO_NUM_3)
+
+#define UART UART_NUM_2
+
+int num = 0;
+
+#define  PIN_FOTOTRANSISTOR GPIO_NUM_34
+#define PIN_SALIDA GPIO_NUM_4
+
+#define ADC_CHANNEL ADC1_CHANNEL_6
+#define ADC_RESOLUTION ADC_WIDTH_BIT_12
+
+DeviceAddress tempSensors[2];
+
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+#define TDS_ANALOG_GPIO     ADC1_CHANNEL_6
+#define TDS_STABILISATION_DELAY     10  
+#define TDS_NUM_SAMPLES             10  
+#define TDS_SAMPLE_PERIOD           20  
+#define TDS_TEMPERATURE             18.0  
+#define TDS_VREF                    1.18  
+#define RES2 820.0
+#define ECREF 200.0
+
+float _rawEC = 0;
+float _kvalue = 1.0;
+float _kvalueHigh = 1.5;
+float _kvalueLow = 0.5;
+float _ecvalue = 0;
+
+float sampleDelay = (TDS_SAMPLE_PERIOD / TDS_NUM_SAMPLES) * 1000;
+
+
+static int s_retry_num = 0;
+
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 5) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void init_uart(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+int sendData(const char* logName, const char* data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    return txBytes;
+}
+
+static void tx_task(void *arg)
+{
+    static const char *TX_TASK_TAG = "TX_TASK";
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    while (1) {
+        sendData(TX_TASK_TAG, "Hello world");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "SBC",
+            .password = "30160127",
+	     .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+
+
+
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+esp_mqtt_client_handle_t mqtt_client_init(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = "mqtt://demo.thingsboard.io",
+        .event_handle = mqtt_event_handler,
+        .port = 1883,
+        .username = "RyjPuWq83zjR2pSWXBqH", //token
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+
+    return client;
+}
+
+void mqtt_send_data(esp_mqtt_client_handle_t client, const char *topic, cJSON *data)
+{
+    char *post_data = cJSON_PrintUnformatted(data);
+    esp_mqtt_client_publish(client, topic, post_data, 0, 1, 0);
+    free(post_data);
+}
+
+
+void expose_vref(){
+    // Expose the ADC VREF to a GPIO so we can measure it rather than assume it is 1.1v
+    ESP_ERROR_CHECK(adc2_vref_to_gpio(GPIO_NUM_25));
+    ESP_LOGI(TAG, "VREF routed to ADC1, pin 25\n");
+}
+
+
+float convert_to_ppm(int analogReading, float temperature){
+    ESP_LOGI(TAG, "Converting an analog value to a TDS PPM value.");
+    //https://www.dfrobot.com/wiki/index.php/Gravity:_Analog_TDS_Sensor_/_Meter_For_Arduino_SKU:_SEN0244#More_Documents
+    float adcCompensation = 1 + (1/3.9); // 1/3.9 (11dB) attenuation.
+    float vPerDiv = (TDS_VREF / 4096) * adcCompensation; // Calculate the volts per division using the VREF taking account of the chosen attenuation value.
+    float averageVoltage = analogReading * vPerDiv; // Convert the ADC reading into volts
+    float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationVolatge = averageVoltage / compensationCoefficient;  //temperature compensation
+    float tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge) * 0.5; //convert voltage value to tds value
+
+    ESP_LOGI(TAG, "Volts per division = %f", vPerDiv);
+    ESP_LOGI(TAG, "Average Voltage = %f", averageVoltage);
+    ESP_LOGI(TAG, "Temperature (currently fixed, we should measure this) = %f", temperature);
+    ESP_LOGI(TAG, "Compensation Coefficient = %f", compensationCoefficient);
+    ESP_LOGI(TAG, "Compensation Voltge = %f", compensationVolatge);
+    ESP_LOGI(TAG, "tdsValue = %f ppm", tdsValue);
+    return tdsValue;
+}
+
+
+float convert_to_ec(float voltage, float temperature){
+    float value = 0, valueTemp = 0;
+    _rawEC = 1000 * voltage / RES2 / ECREF;
+    valueTemp = _rawEC * _kvalue;
+
+    if (valueTemp > 2.5) {
+        _kvalue = _kvalueHigh;
+    } else if (valueTemp < 2.0) {
+        _kvalue = _kvalueLow;
+    }
+
+    value = _rawEC * _kvalue; 
+    value = value / (1.0 + 0.0185 * (temperature - 25.0)); 
+    _ecvalue = value;                   
+    return value;
+}
+
+
+
+
+void app_main(void)
+{
+    ds18b20_init(TEMP_BUS);
+    ds18b20_setResolution(tempSensors,2,10);
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
+
+
+    esp_mqtt_client_handle_t client = mqtt_client_init();
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_6 ,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_7 ,ADC_ATTEN_DB_11);
+
+    int raw_ppm,raw_ec;
+    float ppm, ec, cTemp;
+    init_uart();
+    
+    while(1){
+        xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+        cTemp = ds18b20_get_temp();
+        raw_ppm = adc1_get_raw(ADC1_CHANNEL_6);
+        ppm = convert_to_ppm(raw_ppm,cTemp);
+        raw_ec = adc1_get_raw(ADC1_CHANNEL_7);
+        ec = convert_to_ec(raw_ec,cTemp);
+        printf("Temperatura: %f\n",cTemp);
+        printf("PPM  = %f \n", ppm);
+        printf("Electroconductividad  = %f \n", ec);
+        vTaskDelay(500);
+        cJSON *data = cJSON_CreateObject();
+        cJSON_AddNumberToObject(data, "temperature", cTemp);
+        mqtt_send_data(client, "v1/devices/me/telemetry", data);
+
+        cJSON *data2 = cJSON_CreateObject();
+        cJSON_AddNumberToObject(data2, "ppm", ppm);
+        mqtt_send_data(client, "v1/devices/me/telemetry", data2);
+
+        cJSON *data3 = cJSON_CreateObject();
+        cJSON_AddNumberToObject(data3, "k", ec);
+        mqtt_send_data(client, "v1/devices/me/telemetry", data3);
+    }
+}
